d9955723a478f0456a6780733c91a97f6eb65f6b
cristian camilo lópez giraldo <k-milo3110@hotmail.es>
2024-11-02 17:01:38 -0500

    Index Providers

---
+++ Models/Provider.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; } //Será la llave primaria
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
