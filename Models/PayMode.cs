cc3b5d46a56c02b83de86d8752c9ea09386d1655
cristian camilo lópez giraldo <k-milo3110@hotmail.es>
2024-11-03 14:09:14 -0500

    Index Pay Mode

---
+++ Models/PayMode.cs
@@ -0,0 +1,10 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class PayMode
+    {
+        public int Id { get; set; } //Será la llave primaria
+        public string Name { get; set; }
+        public string Cedula { get; set; }
+        public string Phone { get; set; }
+    }
+}
