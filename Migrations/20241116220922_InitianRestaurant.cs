905489acdcecd1850854e48aadb738d7a7285d31
cristian camilo lópez giraldo <k-milo3110@hotmail.es>
2024-11-23 22:31:59 -0500

    Ultima Actualizacion

---
+++ Migrations/20241116220922_InitianRestaurant.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupermarketWEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitianRestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "PayModes",
+                newName: "payment_method");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "payment_method",
+                table: "PayModes",
+                newName: "Name");
+        }
+    }
+}
